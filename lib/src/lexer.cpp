82f2671f63411f9d70a5fac59016fd35c5b281f7
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-07-06 02:06:41 -0300

    Add more definition details to the implementation file '.cpp' of the 'Lexer' class.

--- lib/src/lexer.cpp
+++ lib/src/lexer.cpp
@@ -26,7 +26,13 @@ char Lexer::Advance() {
   return source_code_[current_ - 1];
 }
 
-void Lexer::Comment() { return; }
+void Lexer::Comment() {
+  while (!IsAtEnd() && Peek(0) != '\n') {
+    Advance();
+  }
+
+  return;
+}
 
 void Lexer::Identifier() { return; }
 
@@ -42,29 +48,112 @@ bool Lexer::IsAtEnd() { return current_ >= source_code_.length(); }
 
 bool Lexer::IsDigit(char c) { return (c >= '0' && c <= '9'); }
 
+// TODO: Refactor this specific method, so it can be shorter.
 void Lexer::LexToken() {
   char current_char = Advance();
 
-  switch(current_char){
-    case('('):
-      AddToken(TokenType::kLeftParen);
-      break;
-    case(')'):
-      AddToken(TokenType::kRightParen);
-      break;
-    case('{'):
-      AddToken(TokenType::kLeftBrace);
-      break;
-    case('}'):
-      AddToken(TokenType::kRightBrace);
-      break;
-    case('['):
-      AddToken(TokenType::kLeftBracket);
-      break;
-    case(']'):
-      AddToken(TokenType::kRightBracket);
-      break;
+  switch (current_char) {
+  case ('('):
+    AddToken(TokenType::kLeftParen);
+    break;
+  case (')'):
+    AddToken(TokenType::kRightParen);
+    break;
+  case ('{'):
+    AddToken(TokenType::kLeftBrace);
+    break;
+  case ('}'):
+    AddToken(TokenType::kRightBrace);
+    break;
+  case ('['):
+    AddToken(TokenType::kLeftBracket);
+    break;
+  case (']'):
+    AddToken(TokenType::kRightBracket);
+    break;
+  case (','):
+    AddToken(TokenType::kComma);
+    break;
+  case (':'):
+    AddToken(TokenType::kColon);
+    break;
+  case (';'):
+    AddToken(TokenType::kSemiColon);
+    break;
+  case ('+'):
+    AddToken(TokenType::kPlus);
+    break;
+  case ('-'):
+    AddToken(TokenType::kMinus);
+    break;
+  case ('%'):
+    AddToken(TokenType::kRemainder);
+    break;
+  case ('*'):
+    AddToken(TokenType::kStar);
+    break;
+  case ('/'):
+    if (Match('/')) {
+      Comment();
+    } else {
+      AddToken(TokenType::kSlash);
+    }
+    break;
+  case ('!'):
+    if (Peek(0) != '=') {
+      AddToken(TokenType::kBang);
+    } else {
+      AddToken(TokenType::kBangEqual);
+    }
+    break;
+  case ('='):
+    if (Peek(0) != '=') {
+      AddToken(TokenType::kEqual);
+    } else {
+      AddToken(TokenType::kEqualEqual);
+    }
+    break;
+  case ('>'):
+    if (Peek(0) != '=') {
+      AddToken(TokenType::kGreater);
+    } else {
+      AddToken(TokenType::kGreaterEqual);
+    }
+    break;
+  case ('<'):
+    if (Peek(0) != '=') {
+      AddToken(TokenType::kLess);
+    } else {
+      AddToken(TokenType::kLessEqual);
+    }
+    break;
+  case ('&'):
+    if (Peek(0) == '&') {
+      AddToken(TokenType::kLogicalAnd);
+    } else {
+      // TODO: Report lexical error.
+    }
+    break;
+  case ('|'):
+    if (Peek(0) == '|') {
+      AddToken(TokenType::kLogicalOr);
+    } else {
+      // TODO: Report lexical error.
+    }
+    break;
+    // TODO: Deal with whitespace characters.
+    // TODO: Deal with the 'High Mul' token.
+    // TODO: Deal with the 'String' token.
+  default:
+    // TODO: Deal with identifiers and also keywords from the Eta language.
+    if (IsAlpha(current_char)) {
+      Identifier();
+      // TODO: Deal with integer constants.
+    } else if (IsDigit(current_char)) {
+      Integer();
+    }
   }
+
   return;
 }
 
