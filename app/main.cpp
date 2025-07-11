64c39eafbd000e32bd3ecf5ea3e4aed749cd1b1c
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-07-10 22:04:23 -0300

    Correct formating in the 'main.cpp' file. Add an instance of the 'DiagnosticReporter' class to it.

--- app/main.cpp
+++ app/main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 
-// #include "../lib/include/senbonzakura/diagnostic_reporter.hpp"
+#include "../lib/include/senbonzakura/diagnostic_reporter.hpp"
 #include "../lib/include/senbonzakura/file_scanner.hpp"
 #include "../lib/include/senbonzakura/lexer.hpp"
 
 #include <CLI/CLI.hpp>
 
-int main(int argc, const char** argv){
+int main(int argc, const char **argv) {
   // CLI::App app;
   // CLI11_PARSE(app, argc, argv);
 
-  FileScanner file_scanner{"/home/vmmc2/Senbonzakura/test.eta"};
-  file_scanner.ScanFile();
-  const std::string& file_content = file_scanner.GetFileContent();
+  DiagnosticReporter diagnostic_reporter;
 
-  Lexer lexer{file_content};
+  FileScanner file_scanner{"/home/vmmc2/Senbonzakura/test.eta", diagnostic_reporter};
+  file_scanner.ScanFile();
+  const std::string &file_content = file_scanner.GetFileContent();
+
+  Lexer lexer{file_content, diagnostic_reporter};
   const std::vector<Token> tokens = lexer.LexTokens();
 
-  for(const auto& token : tokens){
-    if(token.token_type_ != TokenType::kFileEnd){
+  for (const auto &token : tokens) {
+    if (token.token_type_ != TokenType::kFileEnd) {
       std::cout << token << std::endl;
     }
   }
