794713fa048f8e65d150ea16ecd2551541db208d
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-07-12 18:26:24 -0300

    Change how the execution of the components from the Senbonzakura compiler is performed within the 'main.cpp' file.

--- app/main.cpp
+++ app/main.cpp
@@ -11,18 +11,24 @@ int main(int argc, const char **argv) {
   // CLI11_PARSE(app, argc, argv);
 
   DiagnosticReporter diagnostic_reporter;
+  std::string file_path = "/home/vmmc2/Senbonzakura/test.eta";
+  FileScanner file_scanner{file_path, diagnostic_reporter};
 
-  FileScanner file_scanner{"/home/vmmc2/Senbonzakura/test.eta", diagnostic_reporter};
   file_scanner.ScanFile();
   const std::string &file_content = file_scanner.GetFileContent();
 
-  Lexer lexer{file_content, diagnostic_reporter};
+  Lexer lexer{file_path, file_content, diagnostic_reporter};
   const std::vector<Token> tokens = lexer.LexTokens();
 
   for (const auto &token : tokens) {
     if (token.token_type_ != TokenType::kFileEnd) {
       std::cout << token << std::endl;
     }
+  }
+  if(diagnostic_reporter.HasFatalErrors()){
+    diagnostic_reporter.PrintDiagnostic();
+    // TODO: Verify if this is the correct exit code for this specific scenario.
+    std::exit(64);
   }
 
   return 0;
