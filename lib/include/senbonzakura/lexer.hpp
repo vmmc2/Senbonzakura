ea3855562f7aac34e1d36d0bc06f055cc1236791
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-07-19 22:04:05 -0300

    Add changes to declaration and definitions of the 'Lexer' class, so it can now deal with a source file from the Eta programming language that is made of Unicode characters encoded using UTF-8.

--- lib/include/senbonzakura/lexer.hpp
+++ lib/include/senbonzakura/lexer.hpp
@@ -2,6 +2,7 @@
 
 #include <any>
 #include <map>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -13,27 +14,34 @@ class Lexer {
 private:
   void AddToken(TokenType token_type);
   void AddToken(TokenType token_type, std::any value);
-  char Advance();
+  char32_t Advance();
+  void Character();
   void Comment();
   void Identifier();
   void Integer();
-  bool IsAlpha(char c);
-  bool IsAlphaNumeric(char c);
+  bool IsAlpha(char32_t c);
+  bool IsAlphaNumeric(char32_t c);
   bool IsAtEnd();
-  bool IsDigit(char c);
+  bool IsHexDigit(char32_t c);
+  bool IsDigit(char32_t c);
   void LexToken();
-  bool Match(char expected);
-  char Peek(int offset);
+  bool Match(char32_t expected);
+  char32_t Peek(std::size_t offset);
+  std::optional<char32_t>
+  ProcessSingleEscape(char32_t current_backslash_char,
+                      char32_t next_char_after_backslash);
+  void ReportError(const std::string& message, unsigned int col_offset);
   void String();
   void UpdateColumnNumber();
+  std::string U32StringToUtf8(const std::u32string &u32str);
 
-  int current_ = 0;
-  int start_ = 0;
-  int line_ = 1;
-  int column_ = 1;
-  int column_delta_ = 1;
+  std::size_t current_ = 0;
+  std::size_t start_ = 0;
+  std::size_t line_ = 1;
+  std::size_t column_ = 1;
+  std::size_t column_delta_ = 1;
   std::string file_path_{""};
-  std::string source_code_;
+  std::u32string source_code_codepoints_;
   std::vector<Token> tokens_;
   std::map<std::string, TokenType> keywords_to_tokentypes_ = {
       {"int", TokenType::kInt},       {"bool", TokenType::kBool},
@@ -42,9 +50,11 @@ private:
       {"true", TokenType::kTrue},     {"false", TokenType::kFalse},
       {"length", TokenType::kLength}, {"use", TokenType::kUse},
   };
-  DiagnosticReporter& diagnostic_reporter_;
+  DiagnosticReporter &diagnostic_reporter_;
 
 public:
-  Lexer(const std::string& file_path, const std::string& source_code, DiagnosticReporter& diagnostic_reporter);
+  Lexer(const std::string &file_path,
+        const std::u32string &source_code_codepoints,
+        DiagnosticReporter &diagnostic_reporter);
   const std::vector<Token> &LexTokens();
 };
--- lib/src/lexer.cpp
+++ lib/src/lexer.cpp
@@ -1,15 +1,19 @@
-#include "../include/senbonzakura/lexer.hpp"
+#include "senbonzakura/lexer.hpp"
 
 #include <any>
 #include <format>
+#include <optional>
 #include <string>
 
-#include "../include/senbonzakura/token.hpp"
-#include "senbonzakura/diagnostic_reporter.hpp"
+#include <utf8.h>
 
-Lexer::Lexer(const std::string &file_path, const std::string &source_code,
+#include "senbonzakura/diagnostic_reporter.hpp"
+#include "senbonzakura/token.hpp"
+
+Lexer::Lexer(const std::string &file_path,
+             const std::u32string &source_code_codepoints,
              DiagnosticReporter &diagnostic_reporter)
-    : file_path_(file_path), source_code_(source_code),
+    : file_path_(file_path), source_code_codepoints_(source_code_codepoints),
       diagnostic_reporter_(diagnostic_reporter) {}
 
 void Lexer::AddToken(TokenType token_type) {
@@ -19,18 +23,24 @@ void Lexer::AddToken(TokenType token_type) {
 }
 
 void Lexer::AddToken(TokenType token_type, std::any value) {
-  std::string lexeme{source_code_.substr(start_, current_ - start_)};
+  std::u32string u32_lexeme{
+      source_code_codepoints_.substr(start_, current_ - start_)};
+  std::string lexeme = U32StringToUtf8(u32_lexeme);
   tokens_.emplace_back(Token(line_, column_, token_type, value, lexeme));
 
   return;
 }
 
-char Lexer::Advance() {
+char32_t Lexer::Advance() {
   current_++;
 
-  return source_code_[current_ - 1];
+  return source_code_codepoints_[current_ - 1];
 }
 
+void Lexer::Character() { return; }
+
+// TODO: Verify if it makes sense to add the line: column_delta_ = current_ -
+// start_; at the end of this method.
 void Lexer::Comment() {
   while (!IsAtEnd() && Peek(0) != '\n') {
     Advance();
@@ -44,7 +54,9 @@ void Lexer::Identifier() {
     Advance();
   }
 
-  std::string lexeme{source_code_.substr(start_, current_ - start_)};
+  std::u32string u32_lexeme{
+      source_code_codepoints_.substr(start_, current_ - start_)};
+  std::string lexeme = U32StringToUtf8(u32_lexeme);
   TokenType token_type = TokenType::kIdentifier;
 
   if (keywords_to_tokentypes_.contains(lexeme)) {
@@ -62,7 +74,12 @@ void Lexer::Integer() {
   while (IsDigit(Peek(0))) {
     Advance();
   }
-  std::any value = std::stoi(source_code_.substr(start_, current_ - start_));
+
+  std::u32string u32_num_str{
+      source_code_codepoints_.substr(start_, current_ - start_)};
+  std::string num_str = U32StringToUtf8(u32_num_str);
+  std::any value = std::stoi(num_str);
+
   AddToken(TokenType::kInteger, value);
 
   column_delta_ = current_ - start_;
@@ -70,61 +87,67 @@ void Lexer::Integer() {
   return;
 }
 
-bool Lexer::IsAlpha(char c) {
-  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+bool Lexer::IsAlpha(char32_t c) {
+  return (c >= U'a' && c <= U'z') || (c >= U'A' && c <= U'Z');
 }
 
-bool Lexer::IsAlphaNumeric(char c) {
-  return (IsAlpha(c) || IsDigit(c) || c == '_' || c == '\'');
+bool Lexer::IsAlphaNumeric(char32_t c) {
+  return (IsAlpha(c) || IsDigit(c) || c == U'_' || c == U'\'');
 }
 
-bool Lexer::IsAtEnd() { return current_ >= source_code_.length(); }
+bool Lexer::IsAtEnd() { return current_ >= source_code_codepoints_.length(); }
 
-bool Lexer::IsDigit(char c) { return (c >= '0' && c <= '9'); }
+bool Lexer::IsDigit(char32_t c) { return (c >= U'0' && c <= U'9'); }
 
+bool Lexer::IsHexDigit(char32_t c) {
+  return (c >= U'0' && c <= U'9') || (c >= U'a' && c <= U'f') ||
+         (c >= U'A' && c <= U'F');
+}
+
+// TODO: Take a look at the error message at the end of this method and fix it.
 void Lexer::LexToken() {
-  char current_char = Advance();
+  char32_t current_char = Advance();
   column_delta_ = 1;
 
   switch (current_char) {
-  case ('('):
+  case (U'('):
     AddToken(TokenType::kLeftParen);
     break;
-  case (')'):
+  case (U')'):
     AddToken(TokenType::kRightParen);
     break;
-  case ('{'):
+  case (U'{'):
     AddToken(TokenType::kLeftBrace);
     break;
-  case ('}'):
+  case (U'}'):
     AddToken(TokenType::kRightBrace);
     break;
-  case ('['):
+  case (U'['):
     AddToken(TokenType::kLeftBracket);
     break;
-  case (']'):
+  case (U']'):
     AddToken(TokenType::kRightBracket);
     break;
-  case (','):
+  case (U','):
     AddToken(TokenType::kComma);
     break;
-  case (':'):
+  case (U':'):
     AddToken(TokenType::kColon);
     break;
-  case (';'):
+  case (U';'):
     AddToken(TokenType::kSemiColon);
     break;
-  case ('+'):
+  case (U'+'):
     AddToken(TokenType::kPlus);
     break;
-  case ('-'):
+  case (U'-'):
     AddToken(TokenType::kMinus);
     break;
-  case ('%'):
+  case (U'%'):
     AddToken(TokenType::kRemainder);
     break;
-  case ('*'):
-    if (Peek(0) == '>' && Peek(1) == '>') {
+  case (U'*'):
+    if (Peek(0) == U'>' && Peek(1) == U'>') {
       Advance();
       Advance();
       AddToken(TokenType::kHighMul);
@@ -133,66 +156,69 @@ void Lexer::LexToken() {
       AddToken(TokenType::kStar);
     }
     break;
-  case ('/'):
-    if (Match('/')) {
+  case (U'/'):
+    if (Match(U'/')) {
       Comment();
     } else {
       AddToken(TokenType::kSlash);
     }
     break;
-  case ('!'):
-    if (Peek(0) != '=') {
-      AddToken(TokenType::kBang);
-    } else {
+  case (U'!'):
+    if (Peek(0) == U'=') {
       Advance();
       AddToken(TokenType::kBangEqual);
       column_delta_ = 2;
+    } else {
+      AddToken(TokenType::kBang);
     }
     break;
-  case ('='):
-    if (Peek(0) != '=') {
-      AddToken(TokenType::kEqual);
-    } else {
+  case (U'='):
+    if (Peek(0) == U'=') {
       Advance();
       AddToken(TokenType::kEqualEqual);
       column_delta_ = 2;
+    } else {
+      AddToken(TokenType::kEqual);
     }
     break;
-  case ('>'):
-    if (Peek(0) != '=') {
-      AddToken(TokenType::kGreater);
-    } else {
+  case (U'>'):
+    if (Peek(0) == U'=') {
       Advance();
       AddToken(TokenType::kGreaterEqual);
       column_delta_ = 2;
+    } else {
+      AddToken(TokenType::kGreater);
     }
     break;
-  case ('<'):
-    if (Peek(0) != '=') {
-      AddToken(TokenType::kLess);
-    } else {
+  case (U'<'):
+    if (Peek(0) == U'=') {
       Advance();
       AddToken(TokenType::kLessEqual);
       column_delta_ = 2;
+    } else {
+      AddToken(TokenType::kLess);
     }
     break;
-  case ('&'):
+  case (U'&'):
     AddToken(TokenType::kLogicalAnd);
     break;
-  case ('|'):
+  case (U'|'):
     AddToken(TokenType::kLogicalOr);
     break;
-  case (' '):
-  case ('\r'):
-  case ('\t'):
+  case (U' '):
+  case (U'\r'):
+  case (U'\t'):
     break;
-  case ('\n'):
+  case (U'\n'):
     line_++;
     column_ = 1;
     column_delta_ = 0;
     break;
-  case ('"'):
+  case (U'"'):
     String();
+    break;
+  case (U'\''):
+    Character();
     break;
   default:
     if (IsAlpha(current_char)) {
@@ -205,8 +231,8 @@ void Lexer::LexToken() {
               .source_name = file_path_, .line = line_, .column = column_}),
           Severity::kFatal,
           std::format(
-              "[E]: Unrecgonized lexeme present inside the source file: {}.",
-              source_code_[current_]));
+              "[E]: Unrecgonized character present inside the source file: {}.",
+              U32StringToUtf8(std::u32string(1, current_char))));
     }
   }
 
@@ -215,8 +241,8 @@ void Lexer::LexToken() {
   return;
 }
 
-bool Lexer::Match(char expected) {
-  if (IsAtEnd() || (source_code_[current_] != expected)) {
+bool Lexer::Match(char32_t expected) {
+  if (IsAtEnd() || (source_code_codepoints_[current_] != expected)) {
     return false;
   } else {
     Advance();
@@ -224,16 +250,128 @@ bool Lexer::Match(char expected) {
   }
 }
 
-char Lexer::Peek(int offset) {
-  char peeked_char = (current_ + offset) >= source_code_.length()
-                         ? '\0'
-                         : source_code_[current_ + offset];
-  return peeked_char;
+char32_t Lexer::Peek(std::size_t offset) {
+  if ((current_ + offset) >= source_code_codepoints_.length()) {
+    return U'\0';
+  }
+  return source_code_codepoints_[current_ + offset];
+}
+
+std::optional<char32_t>
+Lexer::ProcessSingleEscape(char32_t current_backslash_char,
+                           char32_t next_char_after_backslash) {
+  switch (next_char_after_backslash) {
+  case (U'\\'):
+    // Returns the '\' character.
+    return U'\\';
+  case (U'n'):
+    // Returns a newline character '\n'
+    return U'\n';
+  case (U'\''):
+    // Returns a single-quote character '\''
+    return U'\'';
+  case (U'\"'):
+    // Returns a double-quote character '\"'
+    return U'\"';
+    // TODO: Evaluate if I need to add other escape characters if Eta supports
+    // them:  (ex: \t, \r, \b, \f,
+    // \v) case U't':  return U'\t';
+  // Possible beginning of a: \x{HHHHHH}
+  case (U'x'): {
+    if (Peek(0) == U'{') {
+      // Consumes the '{' character.
+      Advance();
+      std::u32string hex_digits;
+      // Collect hex digits, limiting them by 6 hex digits.
+      while (IsHexDigit(Peek(0)) && hex_digits.length() < 6) {
+        hex_digits += Advance();
+      }
+
+      // Verifies the enclosing of the sequence and if it is valid.
+      if (hex_digits.empty() || Peek(0) != U'}') {
+        diagnostic_reporter_.Report(
+            std::move(SourceCodeLocation{
+                .source_name = file_path_, .line = line_, .column = column_}),
+            Severity::kFatal,
+            std::format("[E]: Invalid or unclosed Unicode escape sequence: "
+                        "\\x{} present within the source file.",
+                        U32StringToUtf8(hex_digits)));
+        return std::nullopt;
+      }
+      if (hex_digits.length() == 0) {
+        diagnostic_reporter_.Report(
+            std::move(SourceCodeLocation{
+                .source_name = file_path_, .line = line_, .column = column_}),
+            Severity::kFatal,
+            std::format("[E]: Empty hexadecimal digits in Unicode escape "
+                        "sequence within the source file."));
+        return std::nullopt;
+      }
+
+      // Consumes the '}' character.
+      Advance();
+
+      try {
+        unsigned long unicode_value =
+            std::stoul(U32StringToUtf8(hex_digits), nullptr, 16);
+        return static_cast<char32_t>(unicode_value);
+      } catch (const std::out_of_range &oor) {
+        diagnostic_reporter_.Report(
+            std::move(SourceCodeLocation{
+                .source_name = file_path_, .line = line_, .column = column_}),
+            Severity::kFatal,
+            std::format("[E]: There is a Unicode escape value 0x{} is out of "
+                        "valid range within the source file.",
+                        U32StringToUtf8(hex_digits)));
+        return std::nullopt;
+      } catch (const std::invalid_argument &ia) {
+        diagnostic_reporter_.Report(
+            std::move(SourceCodeLocation{
+                .source_name = file_path_, .line = line_, .column = column_}),
+            Severity::kFatal,
+            std::format("[E]: Invalid hexadecimal digits in Unicode escape "
+                        "sequence: \\x{} present within the source file.",
+                        U32StringToUtf8(hex_digits)));
+        return std::nullopt;
+      }
+    } else { // É '\x' mas não é '\x{'
+      diagnostic_reporter_.Report(
+          std::move(SourceCodeLocation{
+              .source_name = file_path_, .line = line_, .column = column_}),
+          Severity::kFatal,
+          std::format("[E]: Unknown escape sequence '\\x' (missing opening "
+                      "brace) present within the source file."));
+      return std::nullopt;
+    }
+  }
+  default:
+    diagnostic_reporter_.Report(
+        std::move(SourceCodeLocation{
+            .source_name = file_path_, .line = line_, .column = column_}),
+        Severity::kFatal,
+        std::format(
+            "[E]: Unknown escape sequence present within the source file."));
+    return std::nullopt;
+  }
+}
+
+void Lexer::ReportError(const std::string &message, unsigned int col_offset) {
+  diagnostic_reporter_.Report(
+      std::move(SourceCodeLocation{.source_name = file_path_,
+                                   .line = line_,
+                                   .column = column_ + col_offset}),
+      Severity::kFatal, message);
 }
 
 void Lexer::String() {
-  while (!IsAtEnd() && Peek(0) != '"') {
-    if (Peek(0) == '\n') {
+  std::u32string literal_content;
+
+  while (!IsAtEnd() && Peek(0) != U'"') {
+    char32_t current_char = Advance();
+
+    if (current_char == U'\n') {
+      line_++;
+      column_ = 1;
       diagnostic_reporter_.Report(
           std::move(SourceCodeLocation{
               .source_name = file_path_, .line = line_, .column = column_}),
@@ -242,31 +380,67 @@ void Lexer::String() {
                       "source file."));
       return;
     }
-    Advance();
+
+    if (current_char == U'\\') {
+      if (IsAtEnd()) {
+        diagnostic_reporter_.Report(
+            std::move(SourceCodeLocation{
+                .source_name = file_path_, .line = line_, .column = column_}),
+            Severity::kFatal,
+            std::format("[E]: Unfinished escape sequence at end of the source "
+                        "file: {}.",
+                        file_path_));
+        return;
+      }
+
+      char32_t escape_char_indicator = Advance();
+      std::optional<char32_t> escaped_cp =
+          ProcessSingleEscape(current_char, escape_char_indicator);
+
+      if (escaped_cp) {
+        literal_content += escaped_cp.value();
+      } else {
+        // The method 'ProcessSingleEscape' already reports an error.
+        return;
+      }
+    } else {
+      literal_content += current_char;
+    }
   }
 
-  if (Peek(0) == '"') {
-    Advance();
-    AddToken(TokenType::kString,
-             source_code_.substr(start_ + 1, current_ - start_ - 2));
-  } else {
+  // Verifies if the string literal has been correctly terminated.
+  if (IsAtEnd() || Peek(0) != U'"') {
     diagnostic_reporter_.Report(
         std::move(SourceCodeLocation{
             .source_name = file_path_, .line = line_, .column = column_}),
         Severity::kFatal,
         std::format("[E]: An unterminated string literal was found within the "
-                    "source code."));
+                    "source file."));
+    return;
   }
+
+  // Consumes the enclosing double-quote character: '"'.
+  Advance();
+
+  AddToken(TokenType::kString, literal_content);
 
   column_delta_ = current_ - start_;
 
   return;
 }
 
+// TODO: Check if this is OK.
 void Lexer::UpdateColumnNumber() {
   column_ += column_delta_;
 
   return;
+}
+
+std::string Lexer::U32StringToUtf8(const std::u32string &u32str) {
+  std::string s;
+  s.reserve(u32str.length() * 4); // Optimization: Max of 4 bytes per char32_t.
+  utf8::utf32to8(u32str.begin(), u32str.end(), std::back_inserter(s));
+  return s;
 }
 
 const std::vector<Token> &Lexer::LexTokens() {
@@ -277,6 +451,8 @@ const std::vector<Token> &Lexer::LexTokens() {
       break;
     }
   }
+  // Add a file end token at the end of the token sequence in order to make the
+  // parsing stage easier.
   tokens_.emplace_back(Token(line_, column_, TokenType::kFileEnd, nullptr, ""));
 
   return tokens_;
