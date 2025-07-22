570b5df9a14b38ea6ef9609440fe0d15909c5bd3
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-07-22 20:43:58 -0300

    Add initial structure of the 'CLI11' library to the 'main.cpp' file.

--- app/main.cpp
+++ app/main.cpp
@@ -1,34 +1,68 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+#include <CLI/CLI.hpp>
 
 #include "senbonzakura/diagnostic_reporter.hpp"
 #include "senbonzakura/file_scanner.hpp"
 #include "senbonzakura/lexer.hpp"
 
-#include <CLI/CLI.hpp>
-
 int main(int argc, const char **argv) {
-  // CLI::App app;
-  // CLI11_PARSE(app, argc, argv);
+  std::string generated_diagnostic_directory_path;
+  std::vector<std::string> eta_programs_filepaths;
 
-  DiagnosticReporter diagnostic_reporter;
-  std::string file_path = "/home/vmmc2/Senbonzakura/test.eta";
-  FileScanner file_scanner{file_path, diagnostic_reporter};
+  CLI::App senbonzakura_compiler_app{
+      "This is the Senbonzakura Compiler. A compiler written using C++ 20 for "
+      "the Eta programming language."};
 
-  file_scanner.ScanFile();
-  const std::u32string &file_content = file_scanner.GetFileContentCodepoints();
+  senbonzakura_compiler_app
+      .add_option("-l,--lex", eta_programs_filepaths,
+                  "This command is responsible for performing the lexing stage "
+                  "of the compilation process and output the generated "
+                  "sequence of tokens. Requires the path to at least one "
+                  "'.eta' file. Can deal with multiples, though.")
+      ->required()
+      ->delimiter(' ')
+      ->expected(1, -1);
 
-  Lexer lexer{file_path, file_content, diagnostic_reporter};
-  const std::vector<Token> tokens = lexer.LexTokens();
+  auto d_option = senbonzakura_compiler_app
+                      .add_option("-D,--destiny_directory",
+                                  generated_diagnostic_directory_path,
+                                  "The directory where the generated "
+                                  "diagnostic files will be placed.")
+                      ->needs("--lex");
 
-  for (const auto &token : tokens) {
-    if (token.token_type_ != TokenType::kFileEnd) {
-      std::cout << token << std::endl;
-    }
+  try {
+    CLI11_PARSE(senbonzakura_compiler_app, argc, argv);
+  } catch (const CLI::ParseError &parse_error) {
+    std::cout
+        << std::format(
+               "[Fatal]: An internal error has occurred while trying to parse "
+               "the arguments provided to the Senbonzakura CLI App.")
+        << std::endl;
+    return senbonzakura_compiler_app.exit(parse_error);
   }
-  if (diagnostic_reporter.HasFatalErrors()) {
-    diagnostic_reporter.PrintDiagnostic();
-    // TODO: Verify if this is the correct exit code for this specific scenario.
-    std::exit(64);
+
+  for (const std::string &current_eta_filepath : eta_programs_filepaths) {
+    DiagnosticReporter diagnostic_reporter;
+    FileScanner file_scanner{current_eta_filepath, diagnostic_reporter};
+
+    file_scanner.ScanFile();
+    const std::u32string &file_content =
+        file_scanner.GetFileContentCodepoints();
+
+    Lexer lexer{current_eta_filepath, file_content, diagnostic_reporter};
+    const std::vector<Token> tokens = lexer.LexTokens();
+
+    file_scanner.OutputTokens(current_eta_filepath, tokens);
+
+    if (diagnostic_reporter.HasFatalErrors()) {
+      diagnostic_reporter.PrintDiagnostic();
+      // TODO: Verify if this is the correct exit code for this specific
+      // scenario.
+      std::exit(64);
+    }
   }
 
   return 0;
