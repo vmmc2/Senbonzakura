63d0c60ba2c86d6eac588cf0202da477bcf5a998
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-07-31 22:37:33 -0300

    Change the implementation of the 'TokenTypeToString' to make it more robust.

--- lib/src/token_type.cpp
+++ lib/src/token_type.cpp
@@ -1,18 +1,54 @@
 #include "senbonzakura/token_type.hpp"
 
-std::string TokenTypeToString(TokenType token_type) {
-  static const std::string token_type_strings[] = {
-      "kLeftParen",    "kRightParen",   "kLeftBrace",  "kRightBrace",
-      "kLeftBracket",  "kRightBracket", "kComma",      "kColon",
-      "kSemiColon",    "kPlus",         "kMinus",      "kRemainder",
-      "kStar",         "kSlash",        "kUnderscore", "kBang",
-      "kBangEqual",    "kEqual",        "kEqualEqual", "kGreater",
-      "kGreaterEqual", "kLess",         "kLessEqual",  "kLogicalAnd",
-      "kLogicalOr",    "kHighMul",      "kIdentifier", "kCharacter",
-      "kInteger",      "kString",       "kInt",        "kBool",
-      "kIf",           "kElse",         "kWhile",      "kReturn",
-      "kTrue",         "kFalse",        "kLength",     "kUse",
-      "kFileEnd"};
+#include <unordered_map>
 
-  return token_type_strings[static_cast<int>(token_type)];
+std::string TokenTypeToString(TokenType token_type) {
+  static const std::unordered_map<TokenType, std::string>
+      token_type_to_str_map = {{TokenType::kLeftParen, "kLeftParen"},
+                               {TokenType::kRightParen, "kRightParen"},
+                               {TokenType::kLeftBrace, "kLeftBrace"},
+                               {TokenType::kRightBrace, "kRightBrace"},
+                               {TokenType::kLeftBracket, "kLeftBracket"},
+                               {TokenType::kRightBracket, "kRightBracket"},
+                               {TokenType::kComma, "kComma"},
+                               {TokenType::kColon, "kColon"},
+                               {TokenType::kSemiColon, "kSemiColon"},
+                               {TokenType::kPlus, "kPlus"},
+                               {TokenType::kMinus, "kMinus"},
+                               {TokenType::kRemainder, "kRemainder"},
+                               {TokenType::kStar, "kStar"},
+                               {TokenType::kSlash, "kSlash"},
+                               {TokenType::kUnderscore, "kUnderscore"},
+                               {TokenType::kBang, "kBang"},
+                               {TokenType::kBangEqual, "kBangEqual"},
+                               {TokenType::kEqual, "kEqual"},
+                               {TokenType::kEqualEqual, "kEqualEqual"},
+                               {TokenType::kGreater, "kGreater"},
+                               {TokenType::kGreaterEqual, "kGreaterEqual"},
+                               {TokenType::kLess, "kLess"},
+                               {TokenType::kLessEqual, "kLessEqual"},
+                               {TokenType::kLogicalAnd, "kLogicalAnd"},
+                               {TokenType::kLogicalOr, "kLogicalOr"},
+                               {TokenType::kHighMul, "kHighMul"},
+                               {TokenType::kIdentifier, "kIdentifier"},
+                               {TokenType::kCharacter, "kCharacter"},
+                               {TokenType::kInteger, "kInteger"},
+                               {TokenType::kString, "kString"},
+                               {TokenType::kInt, "kInt"},
+                               {TokenType::kBool, "kBool"},
+                               {TokenType::kIf, "kIf"},
+                               {TokenType::kElse, "kElse"},
+                               {TokenType::kWhile, "kWhile"},
+                               {TokenType::kReturn, "kReturn"},
+                               {TokenType::kTrue, "kTrue"},
+                               {TokenType::kFalse, "kFalse"},
+                               {TokenType::kLength, "kLength"},
+                               {TokenType::kUse, "kUse"},
+                               {TokenType::kFileEnd, "kFileEnd"}};
+
+  if (token_type_to_str_map.contains(token_type)) {
+    return token_type_to_str_map.at(token_type);
+  }
+
+  return "Unknown TokenType";
 }
