dd89aea375a4d82d017a9fb6a5b2e51d6446262b
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-08-03 22:14:30 -0300

    Add initial test related to the 'Token' class.

--- tests/token_test.cpp
+++ tests/token_test.cpp
@@ -1,3 +1,13 @@
 #include "senbonzakura/token.hpp"
+#include "senbonzakura/token_type.hpp"
 
 #include <gtest/gtest.h>
+
+TEST(TokenTest, TokenConstructorTest){
+  Token identifier_token{1, 5, TokenType::kIdentifier, {}, "x"};
+  EXPECT_EQ(identifier_token.line_, 1);
+  EXPECT_EQ(identifier_token.column_, 5);
+  EXPECT_EQ(identifier_token.token_type_, TokenType::kIdentifier);
+  EXPECT_EQ(identifier_token.value_.has_value(), false);
+  EXPECT_EQ(identifier_token.lexeme_, "x");
+}
