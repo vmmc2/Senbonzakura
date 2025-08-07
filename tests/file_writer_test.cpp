9003b30ecbefa968e051ec6d50836f497ab8075a
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-08-06 22:03:07 -0300

    Add a new test file called 'file_writer_test.cpp', compiling tests for the 'FileWriter' class.

--- tests/file_writer_test.cpp
+++ tests/file_writer_test.cpp
@@ -1,3 +1,17 @@
 #include "senbonzakura/file_writer.hpp"
 
+#include <string>
+#include <vector>
+
 #include <gtest/gtest.h>
+
+TEST(FileWriterTest, FileWriterConstructorTest) {
+  std::vector<std::string> programs_filepaths{
+      "/some/path/to/file_1.eta", "/this/is/another/path/to/a/file_2.eta",
+      "/a/third/path/to/yet/another/file_3.eta"};
+  std::string output_directory{"/path/to/the/output/directory/"};
+  FileWriter file_writer{programs_filepaths, output_directory};
+
+  EXPECT_EQ(file_writer.GetProgramFilepaths(), programs_filepaths);
+  EXPECT_EQ(file_writer.GetOutputDirectory(), output_directory);
+}
