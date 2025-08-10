091565fca19e2e3df5489e9f46e65efb199d57a3
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-08-09 23:07:10 -0300

    Add a new test related to the 'FileWriter' class.

--- tests/file_writer_test.cpp
+++ tests/file_writer_test.cpp
@@ -1,36 +1,72 @@
 #include "senbonzakura/file_writer.hpp"
+#include "senbonzakura/token.hpp"
+#include "senbonzakura/token_type.hpp"
 
 #include <filesystem>
+#include <fstream>
 #include <string>
 #include <vector>
 
 #include <gtest/gtest.h>
 
-TEST(FileWriterTest, FileWriterConstructorTest) {
-  std::vector<std::string> programs_filepaths{
-      "/some/path/to/file_1.eta", "/this/is/another/path/to/a/file_2.eta",
-      "/a/third/path/to/yet/another/file_3.eta"};
-  std::string output_directory{"/path/to/the/output/directory/"};
-  FileWriter file_writer{programs_filepaths, output_directory};
-
-  EXPECT_EQ(file_writer.GetProgramFilepaths(), programs_filepaths);
-  EXPECT_EQ(file_writer.GetOutputDirectory(), output_directory);
-}
-
 class FileWriterTest : public ::testing::Test {
 protected:
   // Temporary directory for the tests.
-  std::string temp_dir_path;
+  std::string temp_output_dir_path_;
 
   void SetUp() override {
     // Unique name for the temporary directory.
-    temp_dir_path =
+    temp_output_dir_path_ =
         std::filesystem::temp_directory_path().string() + "/test_output_dir";
-    std::filesystem::create_directory(temp_dir_path);
+    std::filesystem::create_directory(temp_output_dir_path_);
   }
 
   void TearDown() override {
     // Clean the temporary directory and its contents.
-    std::filesystem::remove_all(temp_dir_path);
+    std::filesystem::remove_all(temp_output_dir_path_);
   }
 };
+
+TEST_F(FileWriterTest, FileWriterConstructorTest) {
+  std::vector<std::string> programs_filepaths{
+      "/some/path/to/file_1.eta", "/this/is/another/path/to/a/file_2.eta",
+      "/a/third/path/to/yet/another/file_3.eta"};
+  FileWriter file_writer{programs_filepaths, temp_output_dir_path_};
+
+  EXPECT_EQ(file_writer.GetProgramFilepaths(), programs_filepaths);
+  EXPECT_EQ(file_writer.GetOutputDirectory(), temp_output_dir_path_);
+}
+
+TEST_F(FileWriterTest, WriteLexerOutputSuccessTest) {
+  std::vector<std::string> programs_filepaths{"/home/user/test_1.eta"};
+  FileWriter file_writer{programs_filepaths, temp_output_dir_path_};
+
+  std::vector<Token> tokens{
+      Token{1, 1, TokenType::kIdentifier, {}, "foo"},
+      Token{1, 4, TokenType::kColon, {}, ":"},
+      Token{1, 5, TokenType::kInt, {}, "int"},
+      Token{1, 8, TokenType::kEqual, {}, "="},
+      Token{1, 9, TokenType::kInteger, 9, "9"},
+      Token{1, 10, TokenType::kFileEnd, {}, ""},
+  };
+
+  file_writer.WriteLexerOutput(programs_filepaths[0], tokens);
+
+  std::filesystem::path output_file_path =
+      std::filesystem::path(temp_output_dir_path_) / "test_1.eta.lexed";
+
+  ASSERT_TRUE(std::filesystem::exists(output_file_path));
+
+  std::ifstream input_file_stream{output_file_path};
+  std::string produced_output{
+      (std::istreambuf_iterator<char>(input_file_stream)),
+      std::istreambuf_iterator<char>()};
+
+  std::string expected_output = "[001:001] Type: kIdentifier - Lexeme: "
+                                "foo\n[001:004] Type: kColon - Lexeme: "
+                                ":\n[001:005] Type: kInt - Lexeme: "
+                                "int\n[001:008] Type: kEqual - Lexeme: "
+                                "=\n[001:009] Type: kInteger - Value: 9\n";
+
+  EXPECT_EQ(produced_output, expected_output);
+}
