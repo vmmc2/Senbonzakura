a6e2fb50aa77c7db7ea9bdf73441cf0018167987
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-08-12 22:51:04 -0300

    Add fixture class that will be needed when writing the tests related to the 'FileScanner' class.

--- tests/file_scanner_test.cpp
+++ tests/file_scanner_test.cpp
@@ -1,1 +1,25 @@
 #include "senbonzakura/file_scanner.hpp"
+
+#include <filesystem>
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+class FileScannerTest : public ::testing::Test {
+protected:
+  std::string temp_dir_path_;
+  DiagnosticReporter diagnostic_reporter_;
+
+  void SetUp() override {
+    temp_dir_path_ = std::filesystem::temp_directory_path().string() +
+                     "/test_filescanner_dir";
+    std::filesystem::create_directory(temp_dir_path_);
+  }
+
+  void TearDown() override { std::filesystem::remove_all(temp_dir_path_); }
+
+  void CreateTestFile(const std::string &filename, const std::string &content) {
+    std::ofstream file{temp_dir_path_ + "/" + filename, std::ios::binary};
+    file << content;
+  }
+};
