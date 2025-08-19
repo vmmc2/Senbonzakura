b5f213b70d147bc8ab95c8a6878e15afae391c74
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-08-18 21:01:22 -0300

    Add initial structure of the 'Parser' class.

--- lib/include/senbonzakura/parser.hpp
+++ lib/include/senbonzakura/parser.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <vector>
+
+#include "senbonzakura/diagnostic_reporter.hpp"
+#include "senbonzakura/token.hpp"
+
+class Parser {
+private:
+  std::size_t current_ = 0;
+  std::size_t start_ = 0;
+  std::string file_path_{""};
+  const std::vector<Token> &tokens_;
+  DiagnosticReporter &diagnostic_reporter_;
+
+public:
+  Parser(const std::string &file_path, const std::vector<Token> &tokens,
+         DiagnosticReporter &diagnostic_reporter);
+  void Parse();
+};
--- lib/src/parser.cpp
+++ lib/src/parser.cpp
@@ -1,1 +1,8 @@
 #include "senbonzakura/parser.hpp"
+
+Parser::Parser(const std::string &file_path, const std::vector<Token> &tokens,
+               DiagnosticReporter &diagnostic_reporter)
+    : file_path_{file_path}, tokens_{tokens},
+      diagnostic_reporter_{diagnostic_reporter} {}
+
+void Parse() {}
